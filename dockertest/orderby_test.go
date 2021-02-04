f486265aac7bbc4038e7ee0df6035dfcaf85382d
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-02-05 03:27:14 +0900

    update dockertest

---
+++ dockertest/orderby_test.go
@@ -0,0 +1,68 @@
+package dockertest_test
+
+import (
+	"testing"
+
+	"github.com/champon1020/mgorm"
+	"github.com/champon1020/mgorm/internal"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestOrderBy(t *testing.T) {
+	testCases := []struct {
+		Stmt   *mgorm.Stmt
+		Result *[]Employee
+	}{
+		// SELECT * FROM first_name ORDER BY first_name;
+		{
+			mgorm.Select(db, "first_name").
+				From("employees").
+				OrderBy("first_name", false).(*mgorm.Stmt),
+			&[]Employee{
+				{FirstName: "Anneke"},
+				{FirstName: "Bezalel"},
+				{FirstName: "Chirstian"},
+				{FirstName: "Duangkaew"},
+				{FirstName: "Georgi"},
+				{FirstName: "Kyoichi"},
+				{FirstName: "Parto"},
+				{FirstName: "Saniya"},
+				{FirstName: "Sumant"},
+				{FirstName: "Tzvetan"},
+			},
+		},
+
+		// SELECT * FROM first_name ORDER BY first_name DESC;
+		{
+			mgorm.Select(db, "first_name").
+				From("employees").
+				OrderBy("first_name", true).(*mgorm.Stmt),
+			&[]Employee{
+				{FirstName: "Tzvetan"},
+				{FirstName: "Sumant"},
+				{FirstName: "Saniya"},
+				{FirstName: "Parto"},
+				{FirstName: "Kyoichi"},
+				{FirstName: "Georgi"},
+				{FirstName: "Duangkaew"},
+				{FirstName: "Chirstian"},
+				{FirstName: "Bezalel"},
+				{FirstName: "Anneke"},
+			},
+		},
+	}
+
+	for i, testCase := range testCases {
+		model := new([]Employee)
+		if err := testCase.Stmt.Query(model); err != nil {
+			t.Errorf("Error was occurred: %v", err)
+			t.Errorf("Executed SQL: %s", testCase.Stmt.String())
+			continue
+		}
+		if diff := cmp.Diff(testCase.Result, model); diff != "" {
+			t.Errorf("Got difference with sample %d", i)
+			t.Errorf("Executed SQL: %s", testCase.Stmt.String())
+			internal.PrintTestDiff(t, diff)
+		}
+	}
+}
