757a9167afa10ecbe329c58973a05d226347dc58
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-02-07 22:45:33 +0900

    add dockertest for union

---
+++ dockertest/union_test.go
@@ -0,0 +1,64 @@
+package dockertest_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/champon1020/mgorm"
+	"github.com/champon1020/mgorm/internal"
+	"github.com/google/go-cmp/cmp"
+)
+
+func UnionTest(t *testing.T) {
+	testCases := []struct {
+		Stmt   *mgorm.Stmt
+		Result *[]time.Time
+	}{
+		{
+			mgorm.Select(db, "hire_date AS date").
+				From("employees").
+				Union(mgorm.Select(nil, "from_date AS date").
+					From("salaries").
+					Var(),
+				).
+				Limit(5).(*mgorm.Stmt),
+			&[]time.Time{
+				time.Date(1985, time.February, 16, 0, 0, 0, 0, time.UTC),
+				time.Date(1985, time.November, 21, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.February, 18, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.June, 26, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.August, 28, 0, 0, 0, 0, time.UTC),
+			},
+		},
+		{
+			mgorm.Select(db, "from_date AS date").
+				From("salaries").
+				UnionAll(mgorm.Select(nil, "from_date AS date").
+					From("titles").
+					Var(),
+				).
+				Limit(5).(*mgorm.Stmt),
+			&[]time.Time{
+				time.Date(1985, time.February, 18, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.February, 18, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.June, 26, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.June, 26, 0, 0, 0, 0, time.UTC),
+				time.Date(1986, time.December, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+	}
+
+	for i, testCase := range testCases {
+		model := new([]time.Time)
+		if err := testCase.Stmt.Query(model); err != nil {
+			t.Errorf("Error was occurred: %v", err)
+			t.Errorf("Executed SQL: %s", testCase.Stmt.String())
+			continue
+		}
+		if diff := cmp.Diff(testCase.Result, model); diff != "" {
+			t.Errorf("Got difference with sample %d", i)
+			t.Errorf("Executed SQL: %s", testCase.Stmt.String())
+			internal.PrintTestDiff(t, diff)
+		}
+	}
+}
