f95a5b1eff11597f39b69691207f3fd492c59a87
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-02-12 09:59:13 +0900

    Fix some comments in mgorm package

--- db.go
+++ db.go
@@ -2,94 +2,114 @@ package mgorm
 
 import (
 	"database/sql"
-	"errors"
 	"time"
+
+	"github.com/champon1020/mgorm/errors"
 )
 
-// DB is the db structure.
+// DB is a database handle representing a pool of zero or more underlying connections. It's safe for concurrent use by multiple goroutines.
 type DB struct {
-	db *sql.DB
+	conn *sql.DB
 }
 
 // Ping verifies a connection to the database is still alive, establishing a connection if necessary.
 func (db *DB) Ping() error {
-	if db.db == nil {
-		return errors.New("DB is nil")
+	if db.conn == nil {
+		return errors.New("DB conn is nil", errors.InvalidValueError)
 	}
-	return db.db.Ping()
+	return db.conn.Ping()
+}
+
+// Exec executes a query that doesn't return rows. For example: an INSERT and UPDATE.
+func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return db.conn.Exec(query, args...)
+}
+
+// Query executes a query that returns rows, typically a SELECT.
+func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return db.conn.Query(query, args...)
 }
 
 // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
 func (db *DB) SetConnMaxLifetime(n time.Duration) error {
-	if db.db == nil {
-		return errors.New("DB is nil")
+	if db.conn == nil {
+		return errors.New("DB conn is nil", errors.InvalidValueError)
 	}
-	db.db.SetConnMaxLifetime(n)
+	db.conn.SetConnMaxLifetime(n)
 	return nil
 }
 
 // SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
 func (db *DB) SetMaxIdleConns(n int) error {
-	if db.db == nil {
-		return errors.New("DB is nil")
+	if db.conn == nil {
+		return errors.New("DB conn is nil", errors.InvalidValueError)
 	}
-	db.db.SetMaxIdleConns(n)
+	db.conn.SetMaxIdleConns(n)
 	return nil
 }
 
 // SetMaxOpenConns sets the maximum number of open connections to the database.
 func (db *DB) SetMaxOpenConns(n int) error {
-	if db.db == nil {
-		return errors.New("DB is nil")
+	if db.conn == nil {
+		return errors.New("DB conn is nil", errors.InvalidValueError)
 	}
-	db.db.SetMaxOpenConns(n)
+	db.conn.SetMaxOpenConns(n)
 	return nil
 }
 
-// Close closes the database and prevents new queries from starting.
-// Close then waits for all queries that have started processing on the server to finish.
+// Close closes the database and prevents new queries from starting. Close then waits for all queries that have started processing on the server to finish.
 func (db *DB) Close() error {
-	if db.db == nil {
-		return errors.New("DB is nil")
+	if db.conn == nil {
+		return errors.New("DB conn is nil", errors.InvalidValueError)
 	}
-	return db.db.Close()
+	return db.conn.Close()
 }
 
-// Begin starts a transaction.
+// Begin starts a transaction. The default isolation level is dependent on the driver.
 func (db *DB) Begin() (*Tx, error) {
-	tx, err := db.db.Begin()
+	tx, err := db.conn.Begin()
 	if err != nil {
 		return nil, err
 	}
-	return &Tx{db: db, tx: tx}, nil
+	return &Tx{db: db, conn: tx}, nil
 }
 
-// Tx is structure which handles transaction.
+// Tx is an in-progress database transaction.
 type Tx struct {
-	db *DB
-	tx *sql.Tx
+	db   *DB
+	conn *sql.Tx
 }
 
 // Ping verifies a connection to the database is still alive, establishing a connection if necessary.
 func (tx *Tx) Ping() error {
 	if tx.db == nil {
-		return errors.New("DB is nil")
+		return errors.New("Tx conn is nil", errors.InvalidValueError)
 	}
 	return tx.db.Ping()
 }
 
+// Exec executes a query that doesn't return rows. For example: an INSERT and UPDATE.
+func (tx *Tx) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return tx.conn.Exec(query, args...)
+}
+
+// Query executes a query that returns rows, typically a SELECT.
+func (tx *Tx) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return tx.conn.Query(query, args...)
+}
+
 // Commit commits the transaction.
 func (tx *Tx) Commit() error {
-	if tx.tx == nil {
-		return errors.New("Transaction is nil")
+	if tx.conn == nil {
+		return errors.New("Tx conn is nil", errors.InvalidValueError)
 	}
-	return tx.tx.Commit()
+	return tx.conn.Commit()
 }
 
 // Rollback aborts the transaction.
 func (tx *Tx) Rollback() error {
-	if tx.tx == nil {
-		return errors.New("Transaction is nil")
+	if tx.conn == nil {
+		return errors.New("Tx conn is nil", errors.InvalidValueError)
 	}
-	return tx.tx.Rollback()
+	return tx.conn.Rollback()
 }
--- stmt.go
+++ stmt.go
@@ -11,7 +11,7 @@ import (
 	"github.com/champon1020/mgorm/syntax/cmd"
 )
 
-// Stmt keeps the sql statement.
+// Stmt stores information about query.
 type Stmt struct {
 	db     Pool
 	cmd    syntax.Cmd
@@ -19,16 +19,17 @@ type Stmt struct {
 	errors []error
 }
 
+// call appends called clause.
 func (s *Stmt) call(e syntax.Clause) {
 	s.called = append(s.called, e)
 }
 
+// addError appends occurred error
 func (s *Stmt) addError(err error) {
 	s.errors = append(s.errors, err)
 }
 
-// CaseColumn returns string without double quotes.
-// This is used for CASE WHEN ... statement
+// CaseColumn returns string without double quotes. This is used for CASE WHEN ... statement.
 func (s *Stmt) CaseColumn() string {
 	if _, ok := s.called[0].(*clause.When); ok {
 		sql, err := s.processCaseSQL(true)
@@ -41,8 +42,7 @@ func (s *Stmt) CaseColumn() string {
 	return s.String()
 }
 
-// CaseValue returns string with double quotes.
-// This is used for CASE WHEN ... statement
+// CaseValue returns string with double quotes. This is used for CASE WHEN ... statement.
 func (s *Stmt) CaseValue() string {
 	if _, ok := s.called[0].(*clause.When); ok {
 		sql, err := s.processCaseSQL(false)
@@ -55,13 +55,12 @@ func (s *Stmt) CaseValue() string {
 	return s.String()
 }
 
-// Sub returns Stmt.String with syntax.Sub type.
-// This is used for UNION or WHERE with SELECT clause.
+// Sub returns Stmt.String with syntax.Sub type. This is used for UNION or WHERE with SELECT clause.
 func (s *Stmt) Sub() syntax.Sub {
 	return syntax.Sub(s.String())
 }
 
-// String returns query string.
+// String returns query with string.
 func (s *Stmt) String() string {
 	if _, ok := s.cmd.(*cmd.Select); ok {
 		sql, err := s.processQuerySQL()
@@ -79,8 +78,7 @@ func (s *Stmt) String() string {
 	return sql.String()
 }
 
-// stmtFuncString returns called function like following example.
-//   exmaple: SELECT(...).FROM(...).WHERE(...).QUERY(...).
+// stmtFuncString returns called function like "SELECT(...).FROM(...).WHERE(...).QUERY(...)".
 func (s *Stmt) funcString() string {
 	str := s.cmd.String()
 	for _, e := range s.called {
@@ -89,35 +87,20 @@ func (s *Stmt) funcString() string {
 	return str
 }
 
-// Query executes a query that returns some results.
+// Query executes a query that maps values to model.
 func (s *Stmt) Query(model interface{}) error {
 	if len(s.errors) > 0 {
 		return s.errors[0]
 	}
 
 	switch pool := s.db.(type) {
-	case *DB:
+	case *DB, *Tx:
 		sql, err := s.processQuerySQL()
 		if err != nil {
 			return err
 		}
 
-		rows, err := pool.db.Query(sql.String())
-		if err != nil {
-			return errors.New(err.Error(), errors.DBQueryError)
-		}
-
-		defer rows.Close()
-		if err := internal.MapRowsToModel(rows, model); err != nil {
-			return err
-		}
-	case *Tx:
-		sql, err := s.processQuerySQL()
-		if err != nil {
-			return err
-		}
-
-		rows, err := pool.tx.Query(sql.String())
+		rows, err := pool.Query(sql.String())
 		if err != nil {
 			return errors.New(err.Error(), errors.DBQueryError)
 		}
@@ -141,27 +124,19 @@ func (s *Stmt) Query(model interface{}) error {
 	return nil
 }
 
-// Exec executes a query without returning any results.
+// Exec executes a query without without mapping.
 func (s *Stmt) Exec() error {
 	if len(s.errors) > 0 {
 		return s.errors[0]
 	}
 
 	switch pool := s.db.(type) {
-	case *DB:
+	case *DB, *Tx:
 		sql, err := s.processExecSQL()
 		if err != nil {
 			return err
 		}
-		if _, err := pool.db.Exec(sql.String()); err != nil {
-			return errors.New(err.Error(), errors.DBQueryError)
-		}
-	case *Tx:
-		sql, err := s.processExecSQL()
-		if err != nil {
-			return err
-		}
-		if _, err := pool.tx.Exec(sql.String()); err != nil {
+		if _, err := pool.Exec(sql.String()); err != nil {
 			return errors.New(err.Error(), errors.DBQueryError)
 		}
 	case Mock:
@@ -176,7 +151,7 @@ func (s *Stmt) Exec() error {
 	return nil
 }
 
-// processQuerySQL builds SQL with called expressions.
+// processQuerySQL builds SQL with called clauses.
 func (s *Stmt) processQuerySQL() (internal.SQL, error) {
 	var sql internal.SQL
 
@@ -214,8 +189,8 @@ func (s *Stmt) processQuerySQL() (internal.SQL, error) {
 	return sql, nil
 }
 
-// processCaseSQL builds SQL with called expressions.
-// isColumn flag indicates whether this is called from Stmt.Column() or not.
+// processCaseSQL builds SQL with called clauses.
+// isColumn flag indicates whether this is called from (*Stmt).CaseColumn() or not.
 func (s *Stmt) processCaseSQL(isColumn bool) (internal.SQL, error) {
 	var sql internal.SQL
 	sql.Write("CASE")
@@ -250,7 +225,7 @@ func (s *Stmt) processCaseSQL(isColumn bool) (internal.SQL, error) {
 	return sql, nil
 }
 
-// processQuerySQL builds SQL with called expressions.
+// processQuerySQL builds SQL with called clauses.
 func (s *Stmt) processExecSQL() (internal.SQL, error) {
 	var sql internal.SQL
 
@@ -276,23 +251,23 @@ func (s *Stmt) processExecSQL() (internal.SQL, error) {
 	return sql, nil
 }
 
-// From calls FROM statement.
+// From calls FROM clause.
 func (s *Stmt) From(tables ...string) FromStmt {
 	s.call(clause.NewFrom(tables))
 	return s
 }
 
-// Values calls VALUES statement.
+// Values calls VALUES clause.
 func (s *Stmt) Values(vals ...interface{}) ValuesStmt {
 	s.call(clause.NewValues(vals))
 	return s
 }
 
-// Set calls SET statement.
+// Set calls SET clause.
 func (s *Stmt) Set(vals ...interface{}) SetStmt {
 	u, ok := s.cmd.(*cmd.Update)
 	if !ok {
-		s.addError(errors.New("SET statement can be used with UPDATE command", errors.InvalidValueError))
+		s.addError(errors.New("SET clause can be used with UPDATE command", errors.InvalidValueError))
 		return s
 	}
 	set, err := clause.NewSet(u.Columns, vals)
@@ -304,115 +279,115 @@ func (s *Stmt) Set(vals ...interface{}) SetStmt {
 	return s
 }
 
-// Where calls WHERE statement.
+// Where calls WHERE clause.
 func (s *Stmt) Where(e string, vals ...interface{}) WhereStmt {
 	s.call(clause.NewWhere(e, vals...))
 	return s
 }
 
-// And calls AND statement.
+// And calls AND clause.
 func (s *Stmt) And(e string, vals ...interface{}) AndStmt {
 	s.call(clause.NewAnd(e, vals...))
 	return s
 }
 
-// Or calls OR statement.
+// Or calls OR clause.
 func (s *Stmt) Or(e string, vals ...interface{}) OrStmt {
 	s.call(clause.NewOr(e, vals...))
 	return s
 }
 
-// Limit calls LIMIT statement.
+// Limit calls LIMIT clause.
 func (s *Stmt) Limit(num int) LimitStmt {
 	s.call(clause.NewLimit(num))
 	return s
 }
 
-// Offset calls OFFSET statement.
+// Offset calls OFFSET clause.
 func (s *Stmt) Offset(num int) OffsetStmt {
 	s.call(clause.NewOffset(num))
 	return s
 }
 
-// OrderBy calls ORDER BY statement.
+// OrderBy calls ORDER BY clause.
 func (s *Stmt) OrderBy(col string) OrderByStmt {
 	s.call(clause.NewOrderBy(col, false))
 	return s
 }
 
-// OrderByDesc calls ORDER BY ... DESC statement.
+// OrderByDesc calls ORDER BY ... DESC clause.
 func (s *Stmt) OrderByDesc(col string) OrderByStmt {
 	s.call(clause.NewOrderBy(col, true))
 	return s
 }
 
-// Join calls (INNER) JOIN statement.
+// Join calls (INNER) JOIN clause.
 func (s *Stmt) Join(table string) JoinStmt {
 	s.call(clause.NewJoin(table, clause.InnerJoin))
 	return s
 }
 
-// LeftJoin calls (INNER) JOIN statement.
+// LeftJoin calls (INNER) JOIN clause.
 func (s *Stmt) LeftJoin(table string) JoinStmt {
 	s.call(clause.NewJoin(table, clause.LeftJoin))
 	return s
 }
 
-// RightJoin calls (INNER) JOIN statement.
+// RightJoin calls (INNER) JOIN clause.
 func (s *Stmt) RightJoin(table string) JoinStmt {
 	s.call(clause.NewJoin(table, clause.RightJoin))
 	return s
 }
 
-// FullJoin calls (INNER) JOIN statement.
+// FullJoin calls (INNER) JOIN clause.
 func (s *Stmt) FullJoin(table string) JoinStmt {
 	s.call(clause.NewJoin(table, clause.FullJoin))
 	return s
 }
 
-// On calls ON statement.
+// On calls ON clause.
 func (s *Stmt) On(e string, vals ...interface{}) OnStmt {
 	s.call(clause.NewOn(e, vals...))
 	return s
 }
 
-// Union calls UNION statement.
+// Union calls UNION clause.
 func (s *Stmt) Union(stmt syntax.Sub) UnionStmt {
 	s.call(clause.NewUnion(stmt, false))
 	return s
 }
 
-// UnionAll calls UNION ALL statement.
+// UnionAll calls UNION ALL clause.
 func (s *Stmt) UnionAll(stmt syntax.Sub) UnionStmt {
 	s.call(clause.NewUnion(stmt, true))
 	return s
 }
 
-// GroupBy calls GROUP BY statement.
+// GroupBy calls GROUP BY clause.
 func (s *Stmt) GroupBy(cols ...string) GroupByStmt {
 	s.call(clause.NewGroupBy(cols))
 	return s
 }
 
-// Having calls HAVING statement.
+// Having calls HAVING clause.
 func (s *Stmt) Having(e string, vals ...interface{}) HavingStmt {
 	s.call(clause.NewHaving(e, vals...))
 	return s
 }
 
-// When calls WHEN statement.
+// When calls WHEN clause.
 func (s *Stmt) When(e string, vals ...interface{}) WhenStmt {
 	s.call(clause.NewWhen(e, vals...))
 	return s
 }
 
-// Then calls THEN statement.
+// Then calls THEN clause.
 func (s *Stmt) Then(val interface{}) ThenStmt {
 	s.call(clause.NewThen(val))
 	return s
 }
 
-// Else calls ELSE statement.
+// Else calls ELSE clause.
 func (s *Stmt) Else(val interface{}) ElseStmt {
 	s.call(clause.NewElse(val))
 	return s
