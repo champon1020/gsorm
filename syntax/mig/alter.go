0ebf37276ebb9eb5499fc8cbbc8c6cf23ded34a5
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-02-18 05:24:41 +0900

    Add comments

--- syntax/mig/alter.go
+++ syntax/mig/alter.go
@@ -7,13 +7,15 @@ type AlterTable struct {
 	Table string
 }
 
-func (a *AlterTable) Name() string {
+// Query returns clause keyword.
+func (a *AlterTable) Query() string {
 	return "ALTER TABLE"
 }
 
+// Build makes ALTER TABLE clause with syntax.StmtSet.
 func (a *AlterTable) Build() *syntax.StmtSet {
 	ss := new(syntax.StmtSet)
-	ss.WriteKeyword(a.Name())
+	ss.WriteKeyword(a.Query())
 	ss.WriteValue(a.Table)
 	return ss
 }
