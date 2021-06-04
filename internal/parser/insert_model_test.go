de83aa78d21af446cbd7c5d8cc5c1119d47b8d2e
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2021-06-04 13:33:22 +0900

    fix model parser

--- internal/parser/insert_model_test.go
+++ internal/parser/insert_model_test.go
@@ -1,168 +1,57 @@
 package parser_test
 
 import (
-	"math"
 	"testing"
-	"time"
 
-	"github.com/champon1020/gsorm/internal/parser"
+	"github.com/champon1020/gsorm"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInsertModelParser_ParseMapSlice(t *testing.T) {
-	testCases := []struct {
-		Cols     []string
-		Model    interface{}
-		Expected string
-	}{
-		{
-			[]string{"int", "int8", "int16", "int32", "int64"},
-			&[]map[string]interface{}{
-				{"int": 1, "int8": 127, "int16": 32767, "int32": 2147483647, "int64": 9223372036854775807},
-				{"int": -1, "int8": -128, "int16": -32768, "int32": -2147483648, "int64": -9223372036854775808},
-			},
-			"(1, 127, 32767, 2147483647, 9223372036854775807), (-1, -128, -32768, -2147483648, -9223372036854775808)",
-		},
+func TestInsertModelParser_ParseStruct2(t *testing.T) {
+	type Employee struct {
+		ID        int `gsorm:"emp_no"`
+		FirstName string
 	}
 
-	for _, testCase := range testCases {
-		p, err := parser.NewInsertModelParser(testCase.Cols, testCase.Model)
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		sql, err := p.Parse()
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		assert.Equal(t, testCase.Expected, sql.String())
-	}
+	model := Employee{ID: 1001, FirstName: "Taro"}
+
+	actual := gsorm.Insert(nil, "employees", "emp_no", "first_name").Model(&model).String()
+	expected := `INSERT INTO employees (emp_no, first_name) VALUES (1001, 'Taro')`
+
+	assert.Equal(t, expected, actual)
 }
 
 func TestInsertModelParser_ParseStructSlice(t *testing.T) {
-	testCases := []struct {
-		Cols     []string
-		Model    interface{}
-		Expected string
-	}{
-		{
-			[]string{"int", "int8", "int16", "int32", "int64"},
-			&[]IntModel{
-				{I: 1, I8: 127, I16: 32767, I32: 2147483647, I64: 9223372036854775807},
-				{I: -1, I8: -128, I16: -32768, I32: -2147483648, I64: -9223372036854775808},
-			},
-			"(1, 127, 32767, 2147483647, 9223372036854775807), (-1, -128, -32768, -2147483648, -9223372036854775808)",
-		},
+	type Employee struct {
+		ID        int `gsorm:"emp_no"`
+		FirstName string
 	}
 
-	for _, testCase := range testCases {
-		p, err := parser.NewInsertModelParser(testCase.Cols, testCase.Model)
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		sql, err := p.Parse()
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		assert.Equal(t, testCase.Expected, sql.String())
-	}
+	model := []Employee{{ID: 1001, FirstName: "Taro"}, {ID: 1002, FirstName: "Jiro"}}
+
+	actual := gsorm.Insert(nil, "employees", "emp_no", "first_name").Model(&model).String()
+	expected := `INSERT INTO employees (emp_no, first_name) VALUES (1001, 'Taro'), (1002, 'Jiro')`
+
+	assert.Equal(t, expected, actual)
 }
 
 func TestInsertModelParser_ParseMap(t *testing.T) {
-	testCases := []struct {
-		Cols     []string
-		Model    interface{}
-		Expected string
-	}{
-		{
-			[]string{"int", "int8", "int16", "int32", "int64"},
-			&map[string]interface{}{
-				"int":   -9223372036854775808,
-				"int8":  127,
-				"int16": 32767,
-				"int32": 2147483647,
-				"int64": 9223372036854775807,
-			},
-			"(-9223372036854775808, 127, 32767, 2147483647, 9223372036854775807)",
-		},
-		{
-			[]string{"float32", "float64"},
-			&map[string]interface{}{
-				"float32": float32(math.Pi),
-				"float64": float64(math.Pi),
-			},
-			"(3.1415927, 3.141592653589793)",
-		},
-		{
-			[]string{"bool", "time"},
-			&map[string]interface{}{
-				"bool": true,
-				"time": time.Date(2021, time.January, 2, 3, 4, 5, 0, time.UTC),
-			},
-			"(1, '2021-01-02 03:04:05')",
-		},
-	}
+	model := map[string]interface{}{"emp_no": 1001, "first_name": "Taro"}
 
-	for _, testCase := range testCases {
-		p, err := parser.NewInsertModelParser(testCase.Cols, testCase.Model)
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		sql, err := p.Parse()
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		assert.Equal(t, testCase.Expected, sql.String())
-	}
+	actual := gsorm.Insert(nil, "employees", "emp_no", "first_name").Model(&model).String()
+	expected := `INSERT INTO employees (emp_no, first_name) VALUES (1001, 'Taro')`
+
+	assert.Equal(t, expected, actual)
 }
 
-func TestInsertModelParser_ParseStruct(t *testing.T) {
-	testCases := []struct {
-		Cols     []string
-		Model    interface{}
-		Expected string
-	}{
-		{
-			[]string{"int", "int8", "int16", "int32", "int64"},
-			&IntModel{I8: 127, I16: 32767, I32: 2147483647, I64: 9223372036854775807, I: -9223372036854775808},
-			"(-9223372036854775808, 127, 32767, 2147483647, 9223372036854775807)",
-		},
-		{
-			[]string{"uint", "uint8", "uint16", "uint32", "uint64"},
-			&UintModel{U8: 255, U16: 65535, U32: 4294967295, U64: 18446744073709551615, U: 1},
-			"(1, 255, 65535, 4294967295, 18446744073709551615)",
-		},
-		{
-			[]string{"float32", "float64"},
-			&FloatModel{F32: math.Pi, F64: math.Pi},
-			"(3.1415927, 3.141592653589793)",
-		},
-		{
-			[]string{"bool", "time"},
-			&OtherTypesModel{
-				B:    true,
-				Time: time.Date(2021, time.January, 2, 3, 4, 5, 0, time.UTC),
-			},
-			"(1, '2021-01-02 03:04:05')",
-		},
+func TestInsertModelParser_ParseMapSlice(t *testing.T) {
+	model := []map[string]interface{}{
+		{"emp_no": 1001, "first_name": "Taro"},
+		{"emp_no": 1002, "first_name": "Jiro"},
 	}
 
-	for _, testCase := range testCases {
-		p, err := parser.NewInsertModelParser(testCase.Cols, testCase.Model)
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		sql, err := p.Parse()
-		if err != nil {
-			t.Errorf("Error was occurred: %v", err)
-			continue
-		}
-		assert.Equal(t, testCase.Expected, sql.String())
-	}
+	actual := gsorm.Insert(nil, "employees", "emp_no", "first_name").Model(&model).String()
+	expected := `INSERT INTO employees (emp_no, first_name) VALUES (1001, 'Taro'), (1002, 'Jiro')`
+
+	assert.Equal(t, expected, actual)
 }
